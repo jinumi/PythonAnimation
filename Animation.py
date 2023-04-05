9207383d0dcfdab335cb66be5ae67665c3d3ea6f
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-04-05 17:10:10 +0500

    Modernization Update

--- Animation.py
+++ Animation.py
@@ -1,34 +1,52 @@
+# Importing necessary modules
 from tkinter import *
 from PIL import Image
 
+# Creating the tkinter window
 window = Tk()
+
+# Setting window properties
 window.config(background="white")
 window.geometry("500x500")
 window.resizable(0, 0)
 window.title("Animation")
-window.iconbitmap("icon.ico")
+window.iconbitmap("logo.ico")
 
-file = "yy3.gif"
+# Loading the GIF file and its frames
+file = "animation.gif"
 info = Image.open(file)
 frames = info.n_frames
 im = [PhotoImage(file=file, format=f"gif -index {i}") for i in range(frames)]
+
+# Initializing count and anim variables
 count = 0
 anim = None
 
+# Defining the animation function
+
 
 def animation(count):
-    global anim
-    im2 = im[count]
 
+    global anim
+    # Selecting the current frame
+    im2 = im[count]
+    # Updating the image of the label to show the current frame
     gif_label.configure(image=im2)
+    # Incrementing the count variable to show the next frame in the next iteration
     count += 1
+    # Resetting the count variable to 0 if all frames have been shown
     if count == frames:
         count = 0
+    # Scheduling the next iteration of the animation function after 50 milliseconds
     anim = window.after(50, lambda: animation(count))
 
 
+# Creating a label widget to show the GIF frames
 gif_label = Label(window, image="")
 gif_label.pack()
 
+# Starting the animation
 animation(count)
+
+# Running the main loop of the window
 window.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# PythonAnimation
+# Animation with tkinter
+
+This is a simple Python script that displays an animation using tkinter, a Python GUI library, and PIL (Python Imaging Library).
+
+The animation consists of a series of frames contained in a GIF file. The script loads the file, extracts each frame and displays them in a label widget. The script then cycles through the frames at a specified time interval to give the appearance of an animation.
+
+## Installation
+
+Clone this repository to your local machine
+Ensure you have Python and tkinter installed on your machine
+Install the required packages by running the following command in your terminal:
+
+    pip install pillow
+Run the script using the following command:
+
+    python animation.py
+
+## Usage
+
+The script displays the animation in a tkinter window. The window can be resized, but the animation size is fixed at 500x500 pixels.
+
+## Contributions
+
+Contributions to this repository are welcome! If you find a bug or have a suggestion for improvement, please submit an issue or a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
